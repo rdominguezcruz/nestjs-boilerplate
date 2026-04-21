6209e25d3b229e3ea4185b17dcbd79eac4d50524
Roberto Dominguez Cruz <rdominguezc@hotel-shops.com>
2026-04-21 11:00:31 -0500

    Refactor code structure for improved readability and maintainability

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,18 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('secret-test')
+  getSecretTest() {
+    return {
+      value: process.env.SQL_PASSWORD,
+      source: 'Inyectado por ESO desde Azure Key Vault',
+    };
   }
 }
